1585cc65675f0e1611d0138e98f4a803ba85e97d
yuxuanwu17 <yuxuan.wu17@gmail.com>
2020-10-15 16:12:38 +0800

    1. Finish the ROC plotting 2. Finish the PR Curve

--- auto_plot_curve.py
+++ auto_plot_curve.py
@@ -53,33 +53,59 @@ def main():
 
                 model.load_weights(checkpoint_path)
                 y_score = model.predict(x_test)
+
+                ## PR curve
                 precision[epoch], recall[epoch], _ = precision_recall_curve(y_true=y_test, probas_pred=y_score)
                 average_precision[epoch] = average_precision_score(y_true=y_test, y_score=y_score)
 
-                # print(average_precision)
 
                 ## ROC curve
                 fpr[epoch], tpr[epoch], _ = roc_curve(y_true=y_test, y_score=y_score)
                 roc_auc[epoch] = auc(fpr[epoch], tpr[epoch])
-                name[epoch]='{}_{}_{}'.format(x,y,z)
+                name[epoch]='{}_{}_{}'.format(x.upper(),y,z)
                 epoch = epoch + 1
 
+    ## ROC plotting
     colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
     for i, color in zip(range(epoch), colors):
         plt.plot(fpr[i], tpr[i], color=color, lw=lw,
                  label='ROC curve of {}(area = {:.2f})'
                        ''.format(name[i], roc_auc[i]))
 
-
     plt.plot([0, 1], [0, 1], 'k--', lw=lw)
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
-    plt.title(' ROC plots of EIF3a Full transcripts in different length')
+    plt.title(' ROC plots of EIF3A Full transcripts in different length')
     plt.legend(loc="lower right")
     # plt.show()
     plt.savefig('/home/yuxuan/dp_m6a_org/plot/ROC(RNN_all).png',
+                format='png')
+    plt.cla()
+    plt.figure(figsize=(7, 8))
+
+    ## PR curve plotting
+    colors = cycle(['navy', 'turquoise', 'darkorange', 'cornflowerblue', 'teal'])
+    lines = []
+    labels = []
+
+    for i, color in zip(range(epoch), colors):
+        l, = plt.plot(recall[i], precision[i], color=color, lw=2)
+        lines.append(l)
+        labels.append('Precision-recall for {0} (area = {1:0.2f})'
+                      ''.format(name[i], average_precision[i]))
+
+
+    fig = plt.gcf()
+    fig.subplots_adjust(bottom=0.25)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title('Precision-Recall curve to EIF3A Full transcript in different lengths')
+    plt.legend(lines, labels, loc=(0, -.38), prop=dict(size=14))
+    plt.savefig('/home/yuxuan/dp_m6a_org/plot/PR_Curve(RNN_all).png',
                 format='png')
 
 
